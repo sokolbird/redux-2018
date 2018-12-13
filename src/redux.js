d4aa295dfff1c8609140aa97e070d35d0db21795
Rumyantsev Vitalik <jsruvi@gmail.com>
2018-12-13 15:49:27 +0200

    create store with redux

---
+++ src/redux.js
@@ -0,0 +1,98 @@
+import { createStore, combineReducers } from 'redux';
+
+const createId = () => Math.random();
+
+const initialState = {
+    books: [],
+    readers: [],
+};
+
+const books = (state = [], action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case 'ADD_BOOK':
+            return [...state, { id: createId(), title: payload }];
+        case 'UPDATE_BOOK':
+            return state.map((item) => {
+                if (item.id === payload.id) {
+                    return { ...item, title: payload.newName };
+                }
+
+                return item;
+            });
+        case 'REMOVE_BOOK':
+            return {
+                ...state,
+                books: state.filter(book => book.id !== payload)
+            };
+        default:
+            return state;
+    }
+};
+
+
+const readers = (state = [], action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case 'ADD_READER':
+            return [...state, { id: createId(), title: payload }];
+        case 'UPDATE_READER':
+            return state.map((item) => {
+                if (item.id === payload.id) {
+                    return { ...item, title: payload.newReaderName };
+                }
+
+                return item;
+            });
+        case 'REMOVE_READER':
+            return state.filter(reader => reader.id !== payload);
+        default:
+            return state;
+    }
+};
+
+const reducer = combineReducers({
+    books: books,
+    readers: readers,
+});
+
+const store = createStore(reducer);
+
+const addBook = (bookName) => ({ type: 'ADD_BOOK', payload: bookName });
+const updateBook = (id, newName) => ({ type: 'UPDATE_BOOK', payload: { id, newName } });
+const removeBook = (id) => ({ type: 'REMOVE_BOOK', payload: id });
+
+const addReader = (readerName) => ({ type: 'ADD_READER', payload: readerName });
+const updateReader = (id, newReaderName) => ({ type: 'UPDATE_READER', payload: { id, newReaderName } });
+const removeReader = (id) => ({ type: 'REMOVE_READER', payload: id });
+
+store.subscribe(() => {
+    console.log('From subscribe', store.getState());
+});
+
+
+console.log('------------ initial state');
+
+console.log(store.getState());
+
+console.log('------------- add');
+store.dispatch(addBook('Book 1'));
+store.dispatch(addBook('Book 2'));
+store.dispatch(addBook('Book 3'));
+store.dispatch(addBook('Book 4'));
+
+console.log('------------- update');
+store.dispatch(updateBook(store.getState().books[0].id, 'Updated Book Title'));
+
+console.log('------------- remove');
+store.dispatch(removeBook(store.getState().books[0].id));
+store.dispatch(removeBook(store.getState().books[0].id));
+store.dispatch(removeBook(store.getState().books[0].id));
+store.dispatch(removeBook(store.getState().books[0].id));
+
+
+console.log('---- add reader');
+store.dispatch(addReader('Reader 1'));
+
